80a0272de77f0b60daa9333ebad4f1b4252e6671
Ali Parandeh <43654026+Ali-Parandeh@users.noreply.github.com>
2019-06-23 12:00:36 +0100

    Clearned up, Added Json Loader

--- helper.py
+++ helper.py
@@ -3,11 +3,6 @@
 # REVISED DATE: 
 # PURPOSE: Create utility functions for loading data and preprocessing images.
 
-
-# Magic Commands
-%matplotlib inline
-%config inlineBackend.figure_format = 'retina'
-
 # Import here
 import matplotlib.pyplot as plt
 import numpy as np
@@ -16,18 +11,13 @@ import torch
 from torchvision import datasets, transforms, models
 from args import get_prediction_input_args, get_training_input_args
 
-train_user_input = get_training_input_args()
-predict_user_input = get_prediction_input_args()
-
-# Baskets full of flowers for my valentines day
-data_dir = train_user_input.dir
-train_dir = data_dir + '/train'
-valid_dir = data_dir + '/valid'
-test_dir = data_dir + '/test'
-
 # TODO: Define your transforms for the training, validation, and testing sets
-def data_loader(data_dir, train_dir, valid_dir, test_dir):
+def data_loader(data_dir):
     # I'm just going to put my hands in the training flower basket and shuffle, cut and skew my flowers
+    train_dir = data_dir + '/train'
+    valid_dir = data_dir + '/valid'
+    test_dir = data_dir + '/test'
+
     train_transforms = transforms.Compose([transforms.RandomRotation(30), 
                                           transforms.RandomResizedCrop(224),
                                           transforms.RandomHorizontalFlip(),
@@ -58,8 +48,8 @@ def data_loader(data_dir, train_dir, valid_dir, test_dir):
                                                                batch_size=64, shuffle=True)
     return data, data_loader
 
-data, data_loader =  data_loader(data_dir, train_dir, valid_dir, test_dir)
 
-def load_json()
-with open('cat_to_name.json', 'r') as f:
-    cat_to_name = json.load(f)
+def load_labels():
+    with open('cat_to_name.json', 'r') as f:
+        cat_to_name = json.load(f)
+        return cat_to_name
